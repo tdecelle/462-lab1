1953585b437e58398f1425f2860c41417b5910fd
Thomas DeCelle <tdecelle@gmail.com>
2020-01-16 19:30:07 -0700

    Add hello_monkey rule.

---
+++ out.js
@@ -0,0 +1,73 @@
+module.exports = {
+  "rid": "hello_world",
+  "meta": {
+    "name": "Hello World",
+    "description": "\nA first ruleset for the Quickstart\n",
+    "author": "Phil Windley",
+    "logging": true,
+    "shares": ["hello"]
+  },
+  "global": async function (ctx) {
+    ctx.scope.set("hello", ctx.mkFunction(["obj"], async function (ctx, args) {
+      ctx.scope.set("obj", args["obj"]);
+      ctx.scope.set("msg", await ctx.applyFn(ctx.scope.get("+"), ctx, [
+        "Hello ",
+        ctx.scope.get("obj")
+      ]));
+      return ctx.scope.get("msg");
+    }));
+  },
+  "rules": {
+    "hello_world": {
+      "name": "hello_world",
+      "select": {
+        "graph": { "echo": { "hello": { "expr_0": true } } },
+        "state_machine": {
+          "start": [[
+              "expr_0",
+              "end"
+            ]]
+        }
+      },
+      "body": async function (ctx, runAction, toPairs) {
+        var fired = true;
+        if (fired) {
+          await runAction(ctx, void 0, "send_directive", [
+            "say",
+            { "something": "Hello World" }
+          ], []);
+        }
+        if (fired)
+          ctx.emit("debug", "fired");
+        else
+          ctx.emit("debug", "not fired");
+      }
+    },
+    "hello_monkey": {
+      "name": "hello_monkey",
+      "select": {
+        "graph": { "echo": { "monkey": { "expr_0": true } } },
+        "state_machine": {
+          "start": [[
+              "expr_0",
+              "end"
+            ]]
+        }
+      },
+      "body": async function (ctx, runAction, toPairs) {
+        var fired = true;
+        if (fired) {
+          await runAction(ctx, void 0, "send_directive", [
+            "say",
+            { "something": "Hello Monkey" }
+          ], []);
+        }
+        if (fired)
+          ctx.emit("debug", "fired");
+        else
+          ctx.emit("debug", "not fired");
+      }
+    }
+  }
+};
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsInNvdXJjZXNDb250ZW50IjpbXX0=
