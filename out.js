a2f8e3be45439558ea3f63bf4df28f0897095cbb
Thomas DeCelle <tdecelle@gmail.com>
2020-01-16 21:30:46 -0700

    Finish project.

--- out.js
+++ out.js
@@ -56,10 +56,7 @@ module.exports = {
       },
       "body": async function (ctx, runAction, toPairs) {
         ctx.scope.set("name", await ctx.applyFn(ctx.scope.get("head"), ctx, [await ctx.applyFn(ctx.scope.get("klog"), ctx, [
-            await ctx.applyFn(ctx.scope.get("defaultsTo"), ctx, [
-              await ctx.applyFn(await ctx.modules.get(ctx, "event", "attr"), ctx, ["name"]),
-              "Monkey"
-            ]),
+            await ctx.applyFn(await ctx.modules.get(ctx, "event", "attr"), ctx, ["name"]) || "Monkey",
             "Name: "
           ])]));
         var fired = true;
